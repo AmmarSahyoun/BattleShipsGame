af17e46f51dcd981408937a49f47c043bd4cbf2c
Ammar Sahyoun <ammarzgr@hotmail.com>
2020-01-12 13:23:16 +0100

    creating players and map

---
+++ src/com/company/Player.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Random;
+
+public abstract class Player {
+
+    protected Map map;
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+        map = new Map();
+    }
+
+    public abstract Position getShotCoordinates();
+
+    public void placeShips() {
+        Random rand = new Random();
+        int[] shipSizes = {3, 4};
+        Position startPos;
+
+        for (int size : shipSizes) {
+
+            do {
+                int x = rand.nextInt(Map.GRID_SIZE - size + 1);
+                int y = rand.nextInt(Map.GRID_SIZE);
+                startPos = new Position(x, y);
+            } while (map.addShip(startPos, size) == false);
+
+        }
+    }
+
+}
